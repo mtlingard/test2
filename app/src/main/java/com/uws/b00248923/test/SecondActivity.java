49293c2c6169e0021296db6b353e649f2fcae6f8
juan-ramirez <j127dra@gmail.com>
2015-08-10 23:38:52 -0500

    Commit #2

--- app/src/main/java/com/uws/b00248923/test/SecondActivity.java
+++ app/src/main/java/com/uws/b00248923/test/SecondActivity.java
@@ -1,37 +1,22 @@
 package com.uws.b00248923.test;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button book = (Button) findViewById(R.id.button);
-
-        book.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent mIntent = new Intent(getApplicationContext(), SecondActivity.class);
-                startActivity(mIntent);
-            }
-        });
-
+        setContentView(R.layout.activity_second);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        //It works fuck
+        getMenuInflater().inflate(R.menu.menu_second, menu);
         return true;
     }
 
