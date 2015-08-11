98b9e6fb7ce6873ebc672890d3cbe75abb37085f
mtlingard <mark@marklingard.co.uk>
2015-08-11 18:36:07 +0100

    Added some stuff. Buttons work but button on page 2 needs to calculate / store data

--- app/src/main/java/com/uws/b00248923/test/SecondActivity.java
+++ app/src/main/java/com/uws/b00248923/test/SecondActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -36,6 +38,16 @@ public class SecondActivity extends AppCompatActivity {
                 startActivity(mIntent);
             }
         });
+
+//Spinner
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.sites_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
     @Override
